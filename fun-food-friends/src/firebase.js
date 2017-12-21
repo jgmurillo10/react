11672fc472fad6612458d5f03eb338483d411f10
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-12-21 18:08:19 -0500

    template

---
+++ fun-food-friends/src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyAZZ2UaEfeU-eUxDor90fxCp4uex2PsszA",
+    authDomain: "fun-food-friends-a8ffa.firebaseapp.com",
+    databaseURL: "https://fun-food-friends-a8ffa.firebaseio.com",
+    projectId: "fun-food-friends-a8ffa",
+    storageBucket: "",
+    messagingSenderId: "71448623504"
+  };
+firebase.initializeApp(config);
+export default firebase;
